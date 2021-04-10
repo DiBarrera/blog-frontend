ce01a52720ea1b3e6b3b9029021cac79217a27cb
DiBarrera <di_barrera@hotmail.com>
2021-04-10 14:47:45 -0500

    Layouts creados Admin y Basic

---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutAdmin() {
+    return (
+        <Layout>
+            <h2>Menu Sider Admin</h2>
+            <div>Contenido tres puntos ...</div>
+            <div>Footer tres puntos ...</div>
+        </Layout>
+    )
+}
---
+++ src/layouts/LayoutBasic.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutBasic() {
+    return (
+        <Layout>
+            <h2>Menu Sider Basic</h2>
+            <div>Content Basic</div>
+            <h5>Footer Basic</h5>
+        </Layout>
+    )
+}
