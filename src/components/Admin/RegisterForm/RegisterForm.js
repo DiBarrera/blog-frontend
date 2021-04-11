6605166cf6e5b798a09a599df7d5ede9baabeddb
DiBarrera <di_barrera@hotmail.com>
2021-04-11 16:42:39 -0500

    Configuración en formulario para ingresar datos, guardando la información del formulario

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -5,8 +5,62 @@ import { UserOutlined, LockOutlined, ArrowRightOutlined } from "@ant-design/icon
 import "./RegisterForm.scss"
 
 export default function RegisterForm() {
+
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+        repeatPassword: "",
+        privacyPolicy: false
+    })
+
+    const changeForm = e => {
+
+        console.log(e)
+        console.log(e.target)
+        console.log(e.target.value)
+        console.log(e.target.name)
+
+        if(e.target.name === "privacyPolicy") {
+            setInputs({
+                ...inputs,
+                [e.target.name]: e.target.checked
+            })
+        } else {
+            setInputs({
+                ...inputs,
+                [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    const register = e => {
+
+        console.log(inputs)
+        // e.preventDefault()
+    }
+    
     return (
-        <Form className="register-form">
+        <Form className="register-form" onFinish={register} onChange={changeForm}>
+            <Form.Item>
+                <Input 
+                    prefix={<ArrowRightOutlined type="user" style={{ color: "rgba(0, 0, 0, .25)" }} />}
+                    type="text"
+                    name="nombre"
+                    placeholder="Nombre"
+                    className="register-form__input"
+                    value={inputs.nombre}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Input 
+                    prefix={<ArrowRightOutlined type="user" style={{ color: "rgba(0, 0, 0, .25)" }} />}
+                    type="text"
+                    name="apellido"
+                    placeholder="Apellido"
+                    className="register-form__input"
+                    value={inputs.apellido}
+                />
+            </Form.Item>
             <Form.Item>
                 <Input 
                     prefix={<UserOutlined type="user" style={{ color: "rgba(0, 0, 0, .25)" }} />}
@@ -14,6 +68,7 @@ export default function RegisterForm() {
                     name="email"
                     placeholder="Correo electronico"
                     className="register-form__input"
+                    value={inputs.email}
                 />
             </Form.Item>
             <Form.Item>
@@ -23,6 +78,7 @@ export default function RegisterForm() {
                     name="password"
                     placeholder="Contraseña"
                     className="register-form__input"
+                    value={inputs.password}
                 />
             </Form.Item>
             <Form.Item>
@@ -32,11 +88,13 @@ export default function RegisterForm() {
                     name="repeatPassword"
                     placeholder="Repetir contraseña"
                     className="register-form__input"
+                    value={inputs.repeatPassword}
                 />
             </Form.Item>
             <Form.Item>
                 <Checkbox
                     name="privacyPolicy"
+                    checked={inputs.privacyPolicy}
                 >
                     He leído y acepto la política de privacidad.
                 </Checkbox>
