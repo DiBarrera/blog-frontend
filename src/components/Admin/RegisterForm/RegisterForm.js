13b06f4498dca522acd4974203eef22740261fef
DiBarrera <di_barrera@hotmail.com>
2021-04-12 04:54:34 -0500

    Funcion para resetear formulario de registro

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -131,10 +131,33 @@ export default function RegisterForm() {
                     notification["success"]({
                         message: result.message
                     })
-                    // resetForm()
+                    resetForm()
                 }
             }
         }
+    }
+
+    const resetForm = () => {
+        const inputs = document.getElementsByTagName("input")
+
+        for(let i = 0; i <inputs.length; i++) {
+            inputs[i].classList.remove("success")
+            inputs[i].classList.remove("error")
+        }
+
+        setInputs({
+            email: "",
+            password: "",
+            repeatPassword: "",
+            privacyPolicy: false
+        })
+
+        setFormValid({
+            email: false,
+            password: false,
+            repeatPassword: false,
+            privacyPolicy: false
+        })
     }
     
     return (
