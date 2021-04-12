bfadd35fcf932736638a08146b16b512aaae2c47
DiBarrera <di_barrera@hotmail.com>
2021-04-12 15:03:13 -0500

    Lógica de AuthProvider hecha, la lógica permite cargar componente de LayoutAdmin (Dashboard) si el usuario esta ingresado, si no hay Token el componente LayoutAdmin (Dashboard) no se carga

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -5,6 +5,7 @@ import {
     refreshAccessTokenApi,
     logout
 } from "../api/auth";
+import jwtDecode from "jwt-decode";
 
 export const AuthContext = createContext()
 
@@ -18,7 +19,39 @@ export default function AuthProvider(props) {
         isLoading: true
     })
 
+    useEffect(() => {
+        checkUserLogin(setUser)
+    }, [])
+
     return <AuthContext.Provider value={user}>
         {children}
     </AuthContext.Provider>
+}
+
+function checkUserLogin(setUser) {
+
+    console.log("¿Esta el usuario ingresado en todas las direcciones del sitio?")
+
+    const accessToken = getAccessTokenApi()
+
+    console.log(`El usuario esta ingresado ---> ${accessToken}`)
+
+    if(!accessToken) {
+        const refreshToken = getRefreshTokenApi()
+
+        if(!refreshToken) {
+            logout()
+            setUser({
+                user: null,
+                isLoading: false
+            })
+        } else {
+            refreshAccessTokenApi(refreshToken)
+        }
+    } else {
+        setUser({
+            isloading: false,
+            user: jwtDecode(accessToken)
+        })
+    }
 }
