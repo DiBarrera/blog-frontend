d1846fff9d9eb34a34656bfcddd42bd8111c5dc0
DiBarrera <di_barrera@hotmail.com>
2021-04-13 16:33:12 -0500

    Usando react dropzone para subir imagen como avatar a usuario, issue al subir imágenes de resolución alta, actualizar el method deprecado URL.createObjectURL por GoogleChrome

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { Avatar, Form, Input, Select, Button, Row, Col } from "antd";
+import { useDropzone } from "react-dropzone";
+import NoAvatar from "../../../../assets/img/png/no-avatar.png";
 
 import "./EditUserForm.scss";
 
 export default function EditUserForm(props) {
 
     const { user } = props
+
+    console.log(props)
+
+    const [avatar, setAvatar] = useState(null)
+
+    console.log(avatar)
     
     return (
-        <div>
+        <div className="edit-user-form">
             <h1>Forulario de dición de usuario</h1>
+            <UploadAvatar avatar={avatar} setAvatar={setAvatar} />
             <h2>{user.email}</h2>
+        </div>
+    )
+}
+
+function UploadAvatar(props) {
+
+    const { avatar, setAvatar } = props
+
+    console.log(props)
+
+    const onDrop = useCallback(
+        acceptedFiles => {
+            const file = acceptedFiles[0]
+            setAvatar({file, preview: URL.createObjectURL(file)})
+        }, [setAvatar]
+    )
+
+    const { getRootProps, getInputProps, isDragActive, } = useDropzone({
+        accept: "imgae/jpeg, image/png",
+        noKeyboard: true,
+        onDrop
+    })
+
+    return (
+        <div className="upload-avatar" {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <Avatar size={150} src={NoAvatar} />
+            ) : (
+                <Avatar size={150} src={avatar ? avatar.preview : NoAvatar} />
+            )}
         </div>
     )
 }
