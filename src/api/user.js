55fcfad0edb560095063cd40bce05dcf04ef4986
DiBarrera <di_barrera@hotmail.com>
2021-04-14 16:17:56 -0500

    Creada las funciones para subir avatar, obtener avatar y actualizar usuario en cliente, no probada su funcionalidad

--- src/api/user.js
+++ src/api/user.js
@@ -163,3 +163,75 @@ export function getUsersActiveApi(token, status) {
             return err.message
         })
 }
+
+export function uploadAvatarApi(token, avatar, userId) {
+
+    const url = `${basePath}/${apiVersion}/upload-avatar/${userId}`
+
+    const formData = new FormData()
+    formData.append("avatar", avatar, avatar.name)
+
+    const params = {
+        method: "PUT",
+        body: formData,
+        headers: {
+            Authorization: token
+        }
+    }
+
+    return fetch(url, params)
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then (result => {
+            console.log(result)
+            return result
+        })
+        .catch(err => {
+            console.log(err)
+            return err.message
+        })
+}
+
+export function getAvatarApi(avatarName) {
+
+    const url = `${basePath}/${apiVersion}/get-avatar/${avatarName}`
+
+    return fetch(url)
+        .then(response => {
+            console.log(response)
+            return response.url
+        })
+        .catch(err => {
+            console.log(err)
+            return err.message
+        })
+}
+
+export function updateUserApi(token, user, userId) {
+
+    const url = `${basePath}/${apiVersion}/update-user/${userId}`
+
+    const params = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    }
+
+    return fetch(url, params)
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(result => {
+            console.log(result)
+            return result
+        })
+        .catch(err => {
+            console.log(err)
+            return err.message
+        })
+}
