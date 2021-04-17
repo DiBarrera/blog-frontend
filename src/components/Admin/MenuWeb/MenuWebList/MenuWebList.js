2c4b52bb9d36b7e3723300eeebfba67dff0979b4
DiBarrera <di_barrera@hotmail.com>
2021-04-17 15:06:54 -0500

    Intentando añadir la funcionalidad de Drag and Drop a los Menús

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from "react";
+import { Switch, List, Button, Modal as ModalAntd, notification } from "antd";
+import Modal from "../../../Modal";
+import DragSortableList from "react-drag-sortable";
+
+import "./MenuWebList.scss";
+
+const { confirm } = ModalAntd
 
 export default function MenuWebList(props) {
     
     const { menu, setReloadMenuWeb } = props
+    const [isVisibleModal, setIsVisibleModal] = useState(false)
+    const [modalTitle, setModalTitle] = useState("")
+
+    useEffect(() => {
+        const listItemsArray = []
+
+        menu.forEach(item => {
+            listItemsArray.push({
+                content: (
+                    <div>
+                        <p>{item.title}</p>
+                    </div>
+                )
+            })
+        })
+    }, [menu])
+
+    const onSort = (sortedList, dropEvent) => {
+        console.log(sortedList)
+    }
 
     console.log(menu)
 
@@ -12,6 +39,14 @@ export default function MenuWebList(props) {
             {menu.map(item => (
                 <p key={item._id}>{item.title}</p>
             ))}
+            <div className="menu-web-list">
+                <div className="menu-web-list__header">
+                    <Button type="primary">Menú menú</Button>
+                </div>
+            </div>
+            <div className="menu-web-list__items">
+                <DragSortableList />
+            </div>
         </div>
     )
 }
