a6bcba53875ecdcda065345ed64252ca7252c8b6
DiBarrera <di_barrera@hotmail.com>
2021-04-18 11:09:56 -0500

    Lógica para actualizar el Menú que se esta editando, condicionales para validar campos, la actualización de datos se refleja en Base de datos

--- src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
+++ src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
@@ -9,36 +9,73 @@ import "./EditMenuWebForm.scss";
 export default function EditMenuWebForm(props) {
     
     const { setIsVisibleModal, setReloadMenuWeb, menu } = props
+    const [menuWebData, setMenuWebData] = useState(menu)
+
+    useEffect(() => {
+        setMenuWebData(menu)
+    }, [menu])
+
+    const editMenu = event => {
+        // event.preventDefault()
+        console.log(menuWebData)
+
+        if(!menuWebData.title || !menuWebData.url) {
+            notification["error"]({
+                message: "Todos los campos son obligatorios"
+            })
+        } else {
+            console.log("Editando menu")
+            const accessToken = getAccessTokenApi()
+            updateMenuApi(accessToken, menuWebData._id, menuWebData)
+                .then(response => {
+                    console.log(response)
+                    notification["success"]({
+                        message: response
+                    })
+                    setIsVisibleModal(false)
+                    setReloadMenuWeb(true)
+                })
+                .catch(() => {
+                    notification["error"]({
+                        message: "Error del servidor al acutalizar el menú"
+                    })
+                })
+        }
+    }
 
     return (
         <div className="edit-menu-web-form">
             {/* <h1>Editando menu {menu.title}</h1> */}
-            <EditForm />
+            <EditForm 
+                menuWebData={menuWebData}
+                setMenuWebData={setMenuWebData}
+                editMenu={editMenu}
+            />
         </div>
     )
 }
 
 function EditForm(props) {
 
-    // const { menuWebData, setMenuWebData, editMenu, menu } = props
-    // console.log(props)
+    const { menuWebData, setMenuWebData, editMenu, menu } = props
+    console.log(props)
 
     return (
-        <Form className="form-edit">
+        <Form className="form-edit" onFinish={editMenu}>
             <Form.Item>
                 <Input 
                     prefix={<EditOutlined />}
                     placeholder="Titulo"
-                    // value={}
-                    // onChange={}
+                    value={menuWebData.title}
+                    onChange={e => setMenuWebData({...menuWebData, title: e.target.value})}
                 />
             </Form.Item>
             <Form.Item>
                 <Input 
                     prefix={<LinkOutlined />}
                     placeholder="URL"
-                    // value={}
-                    // onChange={}
+                    value={menuWebData.url}
+                    onChange={e => setMenuWebData({...menuWebData, url: e.target.value})}
                 />
             </Form.Item>
             <Form.Item>
