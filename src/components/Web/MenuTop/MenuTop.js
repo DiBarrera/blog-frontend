f13ca4ae240605d8f685050d5fe297956e8527d7
DiBarrera <di_barrera@hotmail.com>
2021-04-19 10:05:36 -0500

    Llamando al Menú de la Base de datos, en el archivo MenuTop.js la linea 47 y 56 produce dos errores: Warning: Each child in a list should have a unique key prop… Y Warning: React does not recognize the  prop on a DOM element… Considerar y buscar como modificar el symbolo de (…) de la librería de And

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -2,10 +2,33 @@ import React, { useState, useEffect } from "react";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
 import logoWhite from "../../../assets/img/png/logo-personal.png";
+import { getMenuApi } from "../../../api/menu";
 
 import "./MenuTop.scss";
 
 export default function MenuTop() {
+
+    const [menuData, setMenuData] = useState([])
+
+    console.log(menuData)
+
+    useEffect(() => {
+        getMenuApi()
+            .then(response => {
+                console.log(response)
+                const arrayMenu = []
+                response.menu.forEach(item => {
+                    if(item.active) {
+                        console.log("Enviando Menús a TopMenu")
+                        arrayMenu.push(item)
+                    }
+                    // item.active && arrayMenu.push(item)
+                })
+                setMenuData(arrayMenu)
+            })
+    }, [])
+
+    console.log(menuData)
 
     return (
         <Menu className="menu-top-web" mode="horizontal">
@@ -14,7 +37,28 @@ export default function MenuTop() {
                     <img src={logoWhite} alt="Logo" />
                 </Link>
             </Menu.Item>
-            <Menu.Item className="menu-top-web__item">
+
+            {menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false
+                if(external) {
+                    return (
+                        <Menu.Item key={item._id} className="menu-top-web__item">
+                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                {item.title}
+                            </a>
+                        </Menu.Item>
+                    )
+                }
+
+                return (
+                    <Menu.Item key={item._id} className="menu-top-web__item">
+                        <Link to={item.url}>
+                            {item.title}
+                        </Link>
+                    </Menu.Item>
+                )
+            })}
+            {/* <Menu.Item className="menu-top-web__item">
                 <Link to={"/"}>
                     Home...
                 </Link>
@@ -23,7 +67,7 @@ export default function MenuTop() {
                 <Link to={"/contact"}>
                     Contacto...
                 </Link>
-            </Menu.Item>
+            </Menu.Item> */}
             <div>
                 Social Media...
             </div>
