f7c108687a893cf6c2459a4f73bed4637fc69b09
DiBarrera <di_barrera@hotmail.com>
2021-04-19 22:21:30 -0500

    Concluido componente de Testimonials junto con sus estilos scss

--- src/components/Web/Testimonials/Testimonials.js
+++ src/components/Web/Testimonials/Testimonials.js
@@ -3,6 +3,9 @@ import { Row, Col, Card, Avatar } from "antd";
 import AvatarTestimonial1 from "../../../assets/img/jpg/testimonial-1.jpg";
 import AvatarTestimonial2 from "../../../assets/img/jpg/testimonial-2.jpg";
 import AvatarTestimonial3 from "../../../assets/img/jpg/testimonial-3.jpg";
+import AvatarTestimonial4 from "../../../assets/img/jpg/testimonial-4.jpg";
+import AvatarTestimonial5 from "../../../assets/img/jpg/testimonial-5.jpg";
+import AvatarTestimonial6 from "../../../assets/img/jpg/testimonial-6.jpg";
 
 import "./Testimonials.scss";
 
@@ -21,7 +24,54 @@ export default function Testimonials() {
                 <Col lg={16}>
                     <Row className="row-cards">
                         <Col md={8}>
-                            <CardReview />
+                            <CardReview 
+                                name="Person 1"
+                                subtitle="Person's job name 1"
+                                avatar={AvatarTestimonial1}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <CardReview 
+                                name="Person 2"
+                                subtitle="Person's job name 2"
+                                avatar={AvatarTestimonial2}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <CardReview 
+                                name="Person 3"
+                                subtitle="Person's job name 3"
+                                avatar={AvatarTestimonial3}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="row-cards">
+                        <Col md={8}>
+                            <CardReview 
+                                name="Person 4"
+                                subtitle="Person's job name 4"
+                                avatar={AvatarTestimonial4}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <CardReview 
+                                name="Person 5"
+                                subtitle="Person's job name 5"
+                                avatar={AvatarTestimonial5}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <CardReview 
+                                name="Person 6"
+                                subtitle="Person's job name 6"
+                                avatar={AvatarTestimonial6}
+                                review="Urna id volutpat lacus laoreet non curabitur gravida arcu."
+                            />
                         </Col>
                     </Row>
                 </Col>
