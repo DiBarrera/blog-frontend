c5a7048c847ab13557d86436ea21824920c70f6a
DiBarrera <di_barrera@hotmail.com>
2021-04-22 08:12:05 -0500

    Continuando con la estructura del Formulario con los paquetes de tinymce/tinymce-react y moment, se instala la API KEY de tinymce

--- src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
+++ src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
@@ -69,6 +69,36 @@ function AddEditForm(props) {
                     />
                 </Col>
             </Row>
+
+            <div>
+                <Editor
+                    // onInit={(evt, editor) => editorRef.current = editor}
+                    initialValue="<p>This is the initial content of the editor.</p>"
+                    init={{
+                        height: 500,
+                        menubar: true,
+                        plugins: [
+                            'advlist autolink lists link image charmap print preview anchor',
+                            'searchreplace visualblocks code fullscreen',
+                            'insertdatetime media table paste code help wordcount'
+                        ],
+                        toolbar: 'undo redo | formatselect | ' +
+                        'bold italic backcolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                    }}
+                />
+                {/* <button onClick={log}>Log editor content</button> */}
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="btn-submit"
+                >
+                    {post ? "Actualizar post" : "Crear post"}
+                </Button>
+            </div>
+
         </Form>
     )
 }
